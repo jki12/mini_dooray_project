f6e31ba8045cf9dec2ce908e2fac118a51e6859d
jki12 <sjak0227@naver.com>
2024-02-21 17:58:33 +0900

    init

---
+++ src/main/java/com/nhnacademy/mini/dooray/DoorayApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.mini.dooray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoorayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/mini/dooray/DoorayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.mini.dooray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoorayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
