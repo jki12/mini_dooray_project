a72f8a075098ad77f004fb60511f886fd28ac660
SeinJ <drasein@gmail.com>
2024-02-22 10:51:34 +0900

    temp commit

--- src/main/java/com/nhnacademy/mini/dooray/entity/Task.java
+++ src/main/java/com/nhnacademy/mini/dooray/entity/Task.java
@@ -1,8 +1,9 @@
 package com.nhnacademy.mini.dooray.entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.*;
+
 import java.io.Serializable;
 import java.sql.Timestamp;
 import java.util.List;
