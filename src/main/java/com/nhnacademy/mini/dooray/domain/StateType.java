47b4762502c82746f399212ce5d172bb542d4f4e
dmswl <siddltkfkd@gmail.com>
2024-02-22 11:50:14 +0900

    StateType 추가

---
+++ src/main/java/com/nhnacademy/mini/dooray/domain/StateType.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.mini.dooray.domain;
+
+public enum StateType {
+    // project
+    ACTIVE,      // 활성
+    IDLE,        // 휴면
+    TERMINATED,  // 종료
+
+    // 유저
+    REGISTERED,  // 가입
+    DEACTIVATED, //탈퇴
+    INACTIVATED  // 휴면
+}
